4c210d54a5beba769bf159c2149ded7be6188c22
Glyn Hanmer <glynhanmer@gmail.com>
2016-10-12 22:13:03 +0100

    Initial test. Can make SquareWave and it throws errors

---
+++ GOHWaveform/SquareWave.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOHWaveform
+{
+    public class SquareWave
+    {
+        public const string DutyCycleNegativeMessage = "Negative duty cycle not allowed.";
+        public const string DutyCycleAbove1 = "Duty cycle greather than 1 not allowed.";
+        double dutyCycle;
+
+        public SquareWave(double dutyCycle)
+        {
+            if (dutyCycle < 0.0)
+            {
+                throw new ArgumentOutOfRangeException("dutyCycle", dutyCycle, DutyCycleNegativeMessage);
+            } else if (dutyCycle > 1.0)
+            {
+                throw new ArgumentOutOfRangeException("dutyCycle", dutyCycle, DutyCycleAbove1);
+            }
+            this.dutyCycle = dutyCycle;
+        }
+    }
+}
---
+++ GOHWaveformTests/SquareWaveTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GOHWaveform;
+
+namespace GOHWaveformTests
+{
+    [TestClass]
+    public class SquareWaveTests
+    {
+        [TestMethod]
+        public void Constructor_ReturnsSquareWave()
+        {
+            double[] dutyCycles = { 0.0, 0.5, 1.0 };
+            foreach (var dutyCycle in dutyCycles)
+            {
+                SquareWave sw = new SquareWave(dutyCycle);
+                Assert.IsInstanceOfType(sw, typeof(SquareWave));
+            }
+        }
+
+        [TestMethod]
+        public void Constructor_ThrowsArgumentOutOfRangeException_ForNegativeDutyCycle()
+        {
+            double dutyCycle = -1.0;
+            try
+            {
+                SquareWave sw = new SquareWave(dutyCycle);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Assert.IsInstanceOfType(e, typeof(ArgumentOutOfRangeException));
+                StringAssert.Contains(e.Message, SquareWave.DutyCycleNegativeMessage);
+                return;
+            }
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Constructor_ThrowsArgumentOutOfRangeException_ForDutyCycleGreatherThan1()
+        {
+            double dutyCycle = 1.000001;
+            try
+            {
+                SquareWave sw = new SquareWave(dutyCycle);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Assert.IsInstanceOfType(e, typeof(ArgumentOutOfRangeException));
+                StringAssert.Contains(e.Message, SquareWave.DutyCycleAbove1);
+                return;
+            }
+            Assert.Fail();
+        }
+    }
+}
