55afe6ba9f5a901a189d563625645ebc9fa4ef3d
Glyn Hanmer <glynhanmer@gmail.com>
2016-10-13 17:38:13 +0100

    TriangleWave implements Waveform and throws exceptions for mad dutycycle

--- GOHWaveform/TriangleWave.cs
+++ GOHWaveform/TriangleWave.cs
@@ -1,12 +1,27 @@
-﻿namespace GOHWaveformTests
+﻿using System;
+
+namespace GOHWaveform
 {
-    public class TriangleWave
+    public class TriangleWave : Waveform
     {
         private double dutyCycle;
 
         public TriangleWave(double dutyCycle)
         {
+            if (dutyCycle < 0.0)
+            {
+                throw new ArgumentOutOfRangeException("dutyCycle", dutyCycle, Errors.DutyCycleNegativeMessage);
+            }
+            else if (dutyCycle > 1.0)
+            {
+                throw new ArgumentOutOfRangeException("dutyCycle", dutyCycle, Errors.DutyCycleAbove1);
+            }
             this.dutyCycle = dutyCycle;
+        }
+
+        public double Value(double phase)
+        {
+            return 0.0;
         }
     }
 }
--- GOHWaveformTests/TriangleWaveTests.cs
+++ GOHWaveformTests/TriangleWaveTests.cs
@@ -17,5 +17,53 @@ namespace GOHWaveformTests
                 Assert.IsInstanceOfType(tw, typeof(TriangleWave));
             }
         }
+
+        [TestMethod]
+        public void Constructor_ThrowsArgumentOutOfRangeException_ForNegativeDutyCycle()
+        {
+            double dutyCycle = -1.0;
+            try
+            {
+                TriangleWave tw = new TriangleWave(dutyCycle);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Assert.IsInstanceOfType(e, typeof(ArgumentOutOfRangeException));
+                StringAssert.Contains(e.Message, Errors.DutyCycleNegativeMessage);
+                return;
+            }
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Constructor_ThrowsArgumentOutOfRangeException_ForDutyCycleGreatherThan1()
+        {
+            double dutyCycle = 1.000001;
+            try
+            {
+                TriangleWave sw = new TriangleWave(dutyCycle);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Assert.IsInstanceOfType(e, typeof(ArgumentOutOfRangeException));
+                StringAssert.Contains(e.Message, Errors.DutyCycleAbove1);
+                return;
+            }
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Value_ReturnsDouble()
+        {
+            double dutyCycle = 0.25;
+            double[] phases = { 0.0, 0.2, dutyCycle * 2.0 * Math.PI, 3.0, 2.0 * Math.PI };
+            TriangleWave tw = new TriangleWave(dutyCycle);
+            foreach (double phase in phases)
+            {
+                Assert.IsInstanceOfType(tw.Value(phase), typeof(double));
+            }
+        }
+
+        
     }
 }
